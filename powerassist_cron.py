53d9fd3421685a87780431e07bb5dfd3df627f4b
geludwig <74719181+geludwig@users.noreply.github.com>
2023-09-23 11:12:13 +0200

    Update powerassist_cron.py

--- powerassist_cron.py
+++ powerassist_cron.py
@@ -1,89 +1,158 @@
-# MODULES
-try:
-    import requests
-    import os
-    import paramiko
-    import subprocess
-    import time
-except ModuleNotFoundError as err:
-    print(err, ". Install with 'pip' first!")
-    exit()
 
 # VARIABLES
-deltaTime = 900             # ups runtime left before shutdown in seconds, keep in mind that cron intervall must be smaller than deltaTime
-testFlag = False            # shutdown command will not be sent if set to True, instead 'ls' command is sent to test ssh connection
-vertivHost = '<ip/url>'     # for example '192.168.200.1' or '127.0.0.1' if executed on the same host as the Vertiv instance
-sshHost = '<ip/url>'        # for example '192.168.200.40'
-username = '<usr ssh>'
-password = '<pw ssh>'
-port = 22
+DEBUG = True
+MINIMUMRUNTIMELEFT = 900    # minimum runtime left in seconds
+VERTIVIP = 'ip'
+SSHIP = 'ip'
+SSHUSER = 'user'
+SSHPASSW = 'pw'
+SSHPORT = 22
 
-# FUNCTIONS
-def timeFunc():
-    global currentTime
-    currentTime = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
-    return currentTime
-
-def activeFunc():
-    global hourTime
-    hourTime = time.strftime('%H%M', time.gmtime())
-    return hourTime
-
-def upsFunc():
-    # TEST POWER ASSIST API AVAILABILITY
-    try:
-        responseVertiv = requests.get('http://' + vertivHost + ':8210/api/PowerAssist', timeout=1)
-    except requests.exceptions.Timeout:
-        print(timeFunc() + ' [ERROR] powerassist request timeout')
-        errorVertiv = True
-    except requests.exceptions.RequestException:
-        print(timeFunc() + ' [ERROR] powerassist request error')
-        errorVertiv = True        
-    else:
-        jsondict = responseVertiv.json()
-        isAcPresent = (jsondict[0]['status']['isAcPresent'])
-        runTimeToEmpty = (jsondict[0]['status']['runTimeToEmptyInSeconds'])
-        errorVertiv = False
-    # TEST SSH AVAILABILITY
-    with open(os.devnull, 'w') as DEVNULL:
+# INSTALL MISSING MODULES
+def installModules(module, module_name):
+    i = 0
+    for x in module:
         try:
+            globals()[module_name[i]] = importlib.import_module(x)
+        except:
+            if platform.system() == "Windows":
+                subprocess.call(["py", "-m", "pip", "install", x])
+            elif platform.system() == "Linux":
+                subprocess.call(["python3", "-m", "pip", "install", x])
+            else:
+                print(f'Cannot install module {x}.')
+                raise
+            try:
+                globals()[module_name[i]] = importlib.import_module(x)
+            except:
+                raise
+        i += 1
+
+# CURRENT TIME
+def currentTime():
+    current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
+    return current_time
+
+# HEARTBEAT TIME
+def heartbeatTime():
+    global hour_time
+    hour_time = time.strftime('%H%M', time.gmtime())
+    return hour_time
+
+# HEARTBEAT
+def heartbeat():
+    if heartbeatTime() == '1200':
+        print(currentTime() + ' [INFO] powerassist active')
+
+# CKECK UPS STATUS
+def checkUpsApi():
+    responseVertiv = requests.get('http://' + VERTIVIP + ':8210/api/PowerAssist', timeout=1)
+    jsondict = responseVertiv.json()
+    isAcPresent = (jsondict[0]['status']['isAcPresent'])
+    runTimeToEmpty = (jsondict[0]['status']['runTimeToEmptyInSeconds'])
+
+    isAcPresent = False
+
+    if isAcPresent == False:
+        if runTimeToEmpty > MINIMUMRUNTIMELEFT:
+            runTimeLeft = runTimeToEmpty - MINIMUMRUNTIMELEFT
+            return isAcPresent, runTimeLeft
+        else:
+            return isAcPresent, 0
+    else:
+        return isAcPresent, runTimeToEmpty
+
+# PING SSH IP
+def checkSsh():
+    with open(os.devnull, 'w') as DEVNULL:
+        if platform.system() == "Linux":
             subprocess.check_call(
-            ['ping', '-c', '1', sshHost],
+            ['ping', '-c', '1', SSHIP],
             stdout=DEVNULL,  # suppress output
             stderr=DEVNULL
             )
-            errorSsh = False
-        except subprocess.CalledProcessError:
-            print(timeFunc() + ' [ERROR] ssh request error\n')
-            errorSsh = True
-    # ACTIONS
-    if errorSsh == False and errorVertiv == False:
-        if isAcPresent == False:
-            timeToShutdown = (runTimeToEmpty - deltaTime)
-            print(timeFunc() + ' [WARNING] ac lost, shutdown in ', timeToShutdown,' seconds')
-            if runTimeToEmpty < deltaTime:
-                    # COMMAND ISSUED IF RUNTIME LOWER THAN BATTERY LIFE
-                    print(timeFunc() + ' [ALERT] shutdown now')
-                    ssh = paramiko.SSHClient()
-                    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                    ssh.connect(sshHost, port, username, password)
-                    stdin, stdout, stderr = ssh.exec_command('/sbin/shutdown.sh && /sbin/poweroff')
+        if platform.system() == "Windows":
+            subprocess.check_call(
+            ['ping', SSHIP, '-n', '1'],
+            stdout=DEVNULL,  # suppress output
+            stderr=DEVNULL
+            )
+
+# SSH COMMAND IF UPS RUNTIME LOW
+def sendSsh():
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(SSHIP, SSHPORT, SSHUSER, SSHPASSW)
+    stdin, stdout, stderr = ssh.exec_command('/sbin/shutdown.sh && /sbin/poweroff')
+
+# SEND TCP MESSAGE
+'''
+def sendTcp():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCPIP, TCPPORT))
+    s.send('{"MESSAGE"}' + '\n')
+'''
+    
+# DEBUG MODE
+def sendSshDebug():
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(SSHIP, SSHPORT, SSHUSER, SSHPASSW)
+    stdin, stdout, stderr = ssh.exec_command('ls')
+    outlines=stdout.readlines()
+    resp=''.join(outlines)
+    print(f'{resp}')
+
+# MAIN
+module = ['requests', 'paramiko']
+module_name = ['requests', 'paramiko']
+try:
+    import importlib
+    import subprocess
+    import platform
+    import os
+    import socket
+    import time
+    installModules(module, module_name)
+except Exception as err:
+    print(f'{err}')
+    exit()
+
+try:
+    heartbeat()
+except Exception as err:
+    print(f'{err}')
+    exit()
+
+try:
+    isAcPresent, runTimeLeft = checkUpsApi()
+except Exception as err:
+    print(f'{err}')
+    exit()
+
+try:
+    checkSsh()
+except Exception as err:
+    print(f'{err}')
+    exit()
+
+try:
+    if DEBUG == True:
+        print(f'[DEBUG] ssh test command.')
+        sendSshDebug()
+        if runTimeLeft == 0:
+            print(f'[DEBUG] ssh command sent.')
+        elif isAcPresent == False:
+            print(f'[DEBUG] {str(runTimeLeft)} seconds runtime left.')
         else:
-            activeTime = activeFunc()
-            if activeTime == "1200":
-                print(timeFunc() + ' [INFO] powerassist active')
-
-    # DEBUG MODE
-    if testFlag == True:
-        print(timeFunc() + ' [DEBUG] testmode active, see output for response')
-        if errorSsh == False and errorVertiv == False:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(sshHost, port, username, password)
-            stdin, stdout, stderr = ssh.exec_command('ls')
-            outlines=stdout.readlines()
-            resp=''.join(outlines)
-            print(resp)
-
-### CALLER ###
-upsFunc()
+            print(f'[DEBUG] {str(runTimeLeft)} seconds estimated runtime.')
+    else:
+        if runTimeLeft == 0:
+            sendSsh()
+        elif isAcPresent == False:
+            print(f'{str(runTimeLeft)} seconds runtime left.')
+        else:
+            print(f'{str(runTimeLeft)} seconds estimated runtime.')
+except Exception as err:
+    print(err)
+    exit()
